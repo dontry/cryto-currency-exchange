073072b033475bc9f4e74a8cc7a09793885fa701
dontry <mccoy018@gmail.com>
2019-07-30 23:35:51 +1000

    feat(proxy): add proxy for cors

--- backend/app.js
+++ backend/app.js
@@ -12,6 +12,11 @@ const port = 8686;
 
 // initialize our express app
 const app = express();
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); 
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(
   bodyParser.urlencoded({
     extended: true
